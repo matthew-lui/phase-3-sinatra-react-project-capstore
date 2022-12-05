5d11b3d2c893db43d3782411cba633995fd604d6
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-12-05 17:02:03 -0500

    gems working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
+Store.destroy_all
+Inventory.destroy_all
+puts "creating stores"
+s1 = Store.create(store_name: "Matts Crazy Caps", address: "123 Main St Suite 1", phone_number: "555-456-7890")
+s1 = Store.create(store_name: "Matts Wacky Caps", address: "123 Main St Suite 2", phone_number: "555-456-7891")
+s1 = Store.create(store_name: "Matts Wacky Caps", address: "123 Main St Suite 3", phone_number: "555-456-7892")
+
+
 puts "🌱 Seeding spices..."
-
 # Seed your database here
-
 puts "✅ Done seeding!"
