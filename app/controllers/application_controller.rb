7867a3b5994763cc1f75e6b4ec157a5c1c7fe6e2
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-12-06 15:36:23 -0500

    back and front end connected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  end
+  
+  # Add your routes here
+  get "/stores" do
+    Store.all.to_json
+  end
+
+  get "/inventories" do
+    Inventory.all.to_json
   end
 
 end
--- phase-3-capstore-frontend/src/App.js
+++ phase-3-capstore-frontend/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
+
+const API = "http://localhost:9292/stores"
+const API2=  "http://localhost:9292/inventories"
 function App() {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => console.log(data));
+  }, [])
+
+  useEffect(() => {
+    fetch(API2)
+    .then(res => res.json())
+    .then(data => console.log(data));
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div>
+     hello world
+   </div>
+  )
 }
 
 export default App;
